d0acc0d88ca59248b13da68b0846971f8d3a8f9e
Susan Enneking <susan.enneking@gmail.com>
2020-10-20 13:35:40 -0500

    Requirements warnings

--- provision.sh
+++ provision.sh
@@ -7,6 +7,13 @@ pip3 install django-request-logging
 #following needed for postgres
 apt-get install -y libpq-dev python-dev
 pip3 install psycopg2
+pip3 install cryptography==2.3
+pip3 install requests==2.20.0
+pip3 install httplib2==0.18.0
+pip3 install pyxdg==0.26
+pip3 install twisted==20.3.0
+pip3 install urllib3==1.24.2
+
 #get docker
 wget -qO- https://get.docker.com | sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
